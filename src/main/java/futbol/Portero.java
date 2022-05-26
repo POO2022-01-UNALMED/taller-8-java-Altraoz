b215b0fb961b4d908822a4e2008b759d820a9137
Altraoz <curresty@unal.edu.co>
2022-05-25 22:32:08 -0500

    cuarto papi B)

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -9,4 +9,26 @@ public class Portero extends Futbolista {
         this.golesRecibidos = golesRecibidos;
         this.dorsal = dorsal;
     }
+
+    @Override
+    public boolean jugarConLasManos() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos;
+    }
+
+    @Override
+    public int compareTo(Object portero) {
+        var otroPortero = (Portero) portero;
+        return otroPortero.getGolesRecibidos() - golesRecibidos;
+    }
+
+    public short getGolesRecibidos() {return golesRecibidos;}
+    public void setGolesRecibidos(short golesRecibidos) {this.golesRecibidos = golesRecibidos;}
+
+    public byte getDorsal() {return dorsal;}
+    public void setDorsal(byte dorsal) {this.dorsal = dorsal;}
 }
